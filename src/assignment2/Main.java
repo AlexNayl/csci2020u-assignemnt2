7bbd3889f6170bf9764a2c51a9932c88b5ccf5f4
AlexNayl <alex@DESKTOP-IKTGS3V.localdomain>
2021-03-06 16:35:53 -0500

    ADD: directory choosers

--- src/assignment2/Main.java
+++ src/assignment2/Main.java
@@ -1,18 +1,33 @@
-package assignment2;
+/**
+ * @author Alexander Naylor 100 696 151
+ */
 
+package assignment2;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.stage.DirectoryChooser;
 import javafx.stage.Stage;
+
+import java.io.File;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Assignment 2");
         primaryStage.setScene(new Scene(root, 300, 275));
+
+        //Get test and training directories from user
+        DirectoryChooser directoryChooser = new DirectoryChooser();
+        directoryChooser.setInitialDirectory(new File("."));
+        directoryChooser.setTitle("Choose the training folder.");
+        File trainDirectory = directoryChooser.showDialog(primaryStage);
+        directoryChooser.setTitle("Choose the testing folder.");
+        File testDirectory = directoryChooser.showDialog(primaryStage);
+
         primaryStage.show();
     }
 
---
+++ src/assignment2/TestFile.java
@@ -0,0 +1,43 @@
+package assignment2;
+import java.text.DecimalFormat;
+
+public class TestFile {
+    private String filename;
+    private double spamProbability;
+    private String actualClass;
+
+    public TestFile(String filename, double spamProbability, String actualClass) {
+        this.filename = filename;
+        this.spamProbability = spamProbability;
+        this.actualClass = actualClass;
+    }
+
+    public String getFilename() {
+        return filename;
+    }
+
+    public double getSpamProbability() {
+        return spamProbability;
+    }
+
+    public String getActualClass() {
+        return actualClass;
+    }
+
+    public String getSpamProbRounded(){
+        DecimalFormat df = new DecimalFormat("0.00000");
+        return df.format(this.spamProbability);
+    }
+
+    public void setFilename(String filename) {
+        this.filename = filename;
+    }
+
+    public void setActualClass(String actualClass) {
+        this.actualClass = actualClass;
+    }
+
+    public void setSpamProbability(double spamProbability) {
+        this.spamProbability = spamProbability;
+    }
+}
